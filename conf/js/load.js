b4dbed2f49cfc1acbc4b0cd858441a8cbc492cb3
Yenene <Desktop>
2019-05-16 23:51:07 +0800

    1

---
+++ conf/js/load.js
@@ -0,0 +1,305 @@
+// Simple load background Div;
+window.onload=function(){
+	document.getElementById("background").innerHTML += "\
+		<div class='circle-container'>\
+	    <div class='circle'>\</div>\
+	  </div>\
+	  <div class='circle-container'>\
+	    <div class='circle'>\</div>\
+	  </div>\
+	  <div class='circle-container'>\
+	    <div class='circle'>\</div>\
+	  </div>\
+	  <div class='circle-container'>\
+	    <div class='circle'>\</div>\
+	  </div>\
+	  <div class='circle-container'>\
+	    <div class='circle'>\</div>\
+	  </div>\
+	  <div class='circle-container'>\
+	    <div class='circle'>\</div>\
+	  </div>\
+	  <div class='circle-container'>\
+	    <div class='circle'>\</div>\
+	  </div>\
+	  <div class='circle-container'>\
+	    <div class='circle'>\</div>\
+	  </div>\
+	  <div class='circle-container'>\
+	    <div class='circle'>\</div>\
+	  </div>\
+	  <div class='circle-container'>\
+	    <div class='circle'>\</div>\
+	  </div>\
+	  <div class='circle-container'>\
+	    <div class='circle'>\</div>\
+	  </div>\
+	  <div class='circle-container'>\
+	    <div class='circle'>\</div>\
+	  </div>\
+	  <div class='circle-container'>\
+	    <div class='circle'>\</div>\
+	  </div>\
+	  <div class='circle-container'>\
+	    <div class='circle'>\</div>\
+	  </div>\
+	  <div class='circle-container'>\
+	    <div class='circle'>\</div>\
+	  </div>\
+	  <div class='circle-container'>\
+	    <div class='circle'>\</div>\
+	  </div>\
+	  <div class='circle-container'>\
+	    <div class='circle'>\</div>\
+	  </div>\
+	  <div class='circle-container'>\
+	    <div class='circle'>\</div>\
+	  </div>\
+	  <div class='circle-container'>\
+	    <div class='circle'>\</div>\
+	  </div>\
+	  <div class='circle-container'>\
+	    <div class='circle'>\</div>\
+	  </div>\
+	  <div class='circle-container'>\
+	    <div class='circle'>\</div>\
+	  </div>\
+	  <div class='circle-container'>\
+	    <div class='circle'>\</div>\
+	  </div>\
+	  <div class='circle-container'>\
+	    <div class='circle'>\</div>\
+	  </div>\
+	  <div class='circle-container'>\
+	    <div class='circle'>\</div>\
+	  </div>\
+	  <div class='circle-container'>\
+	    <div class='circle'>\</div>\
+	  </div>\
+	  <div class='circle-container'>\
+	    <div class='circle'>\</div>\
+	  </div>\
+	  <div class='circle-container'>\
+	    <div class='circle'>\</div>\
+	  </div>\
+	  <div class='circle-container'>\
+	    <div class='circle'>\</div>\
+	  </div>\
+	  <div class='circle-container'>\
+	    <div class='circle'>\</div>\
+	  </div>\
+	  <div class='circle-container'>\
+	    <div class='circle'>\</div>\
+	  </div>\
+	  <div class='circle-container'>\
+	    <div class='circle'>\</div>\
+	  </div>\
+	  <div class='circle-container'>\
+	    <div class='circle'>\</div>\
+	  </div>\
+	  <div class='circle-container'>\
+	    <div class='circle'>\</div>\
+	  </div>\
+	  <div class='circle-container'>\
+	    <div class='circle'>\</div>\
+	  </div>\
+	  <div class='circle-container'>\
+	    <div class='circle'>\</div>\
+	  </div>\
+	  <div class='circle-container'>\
+	    <div class='circle'>\</div>\
+	  </div>\
+	  <div class='circle-container'>\
+	    <div class='circle'>\</div>\
+	  </div>\
+	  <div class='circle-container'>\
+	    <div class='circle'>\</div>\
+	  </div>\
+	  <div class='circle-container'>\
+	    <div class='circle'>\</div>\
+	  </div>\
+	  <div class='circle-container'>\
+	    <div class='circle'>\</div>\
+	  </div>\
+	  <div class='circle-container'>\
+	    <div class='circle'>\</div>\
+	  </div>\
+	  <div class='circle-container'>\
+	    <div class='circle'>\</div>\
+	  </div>\
+	  <div class='circle-container'>\
+	    <div class='circle'>\</div>\
+	  </div>\
+	  <div class='circle-container'>\
+	    <div class='circle'>\</div>\
+	  </div>\
+	  <div class='circle-container'>\
+	    <div class='circle'>\</div>\
+	  </div>\
+	  <div class='circle-container'>\
+	    <div class='circle'>\</div>\
+	  </div>\
+	  <div class='circle-container'>\
+	    <div class='circle'>\</div>\
+	  </div>\
+	  <div class='circle-container'>\
+	    <div class='circle'>\</div>\
+	  </div>\
+	  <div class='circle-container'>\
+	    <div class='circle'>\</div>\
+	  </div>\
+	  <div class='circle-container'>\
+	    <div class='circle'>\</div>\
+	  </div>\
+	  <div class='circle-container'>\
+	    <div class='circle'>\</div>\
+	  </div>\
+	  <div class='circle-container'>\
+	    <div class='circle'>\</div>\
+	  </div>\
+	  <div class='circle-container'>\
+	    <div class='circle'>\</div>\
+	  </div>\
+	  <div class='circle-container'>\
+	    <div class='circle'>\</div>\
+	  </div>\
+	  <div class='circle-container'>\
+	    <div class='circle'>\</div>\
+	  </div>\
+	  <div class='circle-container'>\
+	    <div class='circle'>\</div>\
+	  </div>\
+	  <div class='circle-container'>\
+	    <div class='circle'>\</div>\
+	  </div>\
+	  <div class='circle-container'>\
+	    <div class='circle'>\</div>\
+	  </div>\
+	  <div class='circle-container'>\
+	    <div class='circle'>\</div>\
+	  </div>\
+	  <div class='circle-container'>\
+	    <div class='circle'>\</div>\
+	  </div>\
+	  <div class='circle-container'>\
+	    <div class='circle'>\</div>\
+	  </div>\
+	  <div class='circle-container'>\
+	    <div class='circle'>\</div>\
+	  </div>\
+	  <div class='circle-container'>\
+	    <div class='circle'>\</div>\
+	  </div>\
+	  <div class='circle-container'>\
+	    <div class='circle'>\</div>\
+	  </div>\
+	  <div class='circle-container'>\
+	    <div class='circle'>\</div>\
+	  </div>\
+	  <div class='circle-container'>\
+	    <div class='circle'>\</div>\
+	  </div>\
+	  <div class='circle-container'>\
+	    <div class='circle'>\</div>\
+	  </div>\
+	  <div class='circle-container'>\
+	    <div class='circle'>\</div>\
+	  </div>\
+	  <div class='circle-container'>\
+	    <div class='circle'>\</div>\
+	  </div>\
+	  <div class='circle-container'>\
+	    <div class='circle'>\</div>\
+	  </div>\
+	  <div class='circle-container'>\
+	    <div class='circle'>\</div>\
+	  </div>\
+	  <div class='circle-container'>\
+	    <div class='circle'>\</div>\
+	  </div>\
+	  <div class='circle-container'>\
+	    <div class='circle'>\</div>\
+	  </div>\
+	  <div class='circle-container'>\
+	    <div class='circle'>\</div>\
+	  </div>\
+	  <div class='circle-container'>\
+	    <div class='circle'>\</div>\
+	  </div>\
+	  <div class='circle-container'>\
+	    <div class='circle'>\</div>\
+	  </div>\
+	  <div class='circle-container'>\
+	    <div class='circle'>\</div>\
+	  </div>\
+	  <div class='circle-container'>\
+	    <div class='circle'>\</div>\
+	  </div>\
+	  <div class='circle-container'>\
+	    <div class='circle'>\</div>\
+	  </div>\
+	  <div class='circle-container'>\
+	    <div class='circle'>\</div>\
+	  </div>\
+	  <div class='circle-container'>\
+	    <div class='circle'>\</div>\
+	  </div>\
+	  <div class='circle-container'>\
+	    <div class='circle'>\</div>\
+	  </div>\
+	  <div class='circle-container'>\
+	    <div class='circle'>\</div>\
+	  </div>\
+	  <div class='circle-container'>\
+	    <div class='circle'>\</div>\
+	  </div>\
+	  <div class='circle-container'>\
+	    <div class='circle'>\</div>\
+	  </div>\
+	  <div class='circle-container'>\
+	    <div class='circle'>\</div>\
+	  </div>\
+	  <div class='circle-container'>\
+	    <div class='circle'>\</div>\
+	  </div>\
+	  <div class='circle-container'>\
+	    <div class='circle'>\</div>\
+	  </div>\
+	  <div class='circle-container'>\
+	    <div class='circle'>\</div>\
+	  </div>\
+	  <div class='circle-container'>\
+	    <div class='circle'>\</div>\
+	  </div>\
+	  <div class='circle-container'>\
+	    <div class='circle'>\</div>\
+	  </div>\
+	  <div class='circle-container'>\
+	    <div class='circle'>\</div>\
+	  </div>\
+	  <div class='circle-container'>\
+	    <div class='circle'>\</div>\
+	  </div>\
+	  <div class='circle-container'>\
+	    <div class='circle'>\</div>\
+	  </div>\
+	  <div class='circle-container'>\
+	    <div class='circle'>\</div>\
+	  </div>\
+	  <div class='circle-container'>\
+	    <div class='circle'>\</div>\
+	  </div>\
+	  <div class='circle-container'>\
+	    <div class='circle'>\</div>\
+	  </div>\
+	  <div class='circle-container'>\
+	    <div class='circle'>\</div>\
+	  </div>\
+	  <div class='circle-container'>\
+	    <div class='circle'>\</div>\
+	  </div>\
+	  <div class='circle-container'>\
+	    <div class='circle'>\</div>\
+	  </div>";
+	  console.log("Background Load.");
+}
